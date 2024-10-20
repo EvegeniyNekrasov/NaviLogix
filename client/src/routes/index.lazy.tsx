fee2461d6c04d3063d1dce1cbc1ee099f674e8ac
EvegeniyNekrasov <eanekrasovart@gmail.com>
2024-10-20 20:42:50 +0200

    client: adding styled components

--- client/src/routes/index.lazy.tsx
+++ client/src/routes/index.lazy.tsx
@@ -1,9 +1,14 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import Container from "../ui/Container";
 
 export const Route = createLazyFileRoute("/")({
     component: Index,
 });
 
 function Index() {
-    return <span className="text-4xl text-blue-500">Some text</span>;
+    return (
+        <Container>
+            <span className="text-4xl text-blue-500">Some text</span>
+        </Container>
+    );
 }
---
+++ client/src/ui/Container.tsx
@@ -0,0 +1,21 @@
+import type React from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+    background: #c1c1c1;
+    width: 100%;
+    border-radius: 4px;
+    border: 1x solid #c2c2c2;
+    height: fit-content;
+    padding: 20px;
+`;
+
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+    return <StyledContainer>{children}</StyledContainer>;
+};
+
+export default Container;
