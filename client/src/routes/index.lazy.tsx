046553c87559d38473acd3b76c71154ac86097b7
EvegeniyNekrasov <eanekrasovart@gmail.com>
2024-10-21 10:01:50 +0200

    client: adding lucide icons

--- client/src/routes/index.lazy.tsx
+++ client/src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import Container from "@ui/Container";
+import { Camera } from "lucide-react";
 
 export const Route = createLazyFileRoute("/")({
     component: Index,
@@ -8,6 +9,7 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
     return (
         <Container>
+            <Camera />
             <span className="text-4xl text-blue-500">Some text</span>
         </Container>
     );
